44063a6a8142e23027e379fe24cf5a0b30d38c48
Andrea Pierré <kir0ul@users.noreply.github.com>
2018-12-16 23:23:46 +0100

    Decoupled tabs

--- front/src/App.vue
+++ front/src/App.vue
@@ -18,22 +18,22 @@
         <v-icon>insert_chart_outlined</v-icon>
       </v-tab>
 
-      <v-tab-item v-for="i in 3" :id="'tab-' + i" :key="i">
-        <v-card flat>
-          <v-card-text>{{ text }}</v-card-text>
-        </v-card>
+      <v-tab-item v-for="i in 3" :value="'tab-' + i" :key="i">
+        <v-content v-if="i === 1"> <DataTable /> </v-content>
       </v-tab-item>
     </v-tabs>
   </v-app>
 </template>
 
 <script>
+import DataTable from "./components/DataTable";
+
 export default {
   data() {
-    return {
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    };
+    return {};
+  },
+  components: {
+    DataTable
   }
 };
 </script>
